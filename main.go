79f50091d572c6f272646494cead7d77f5562d77
kurtka-bein <133507984+kurtka-bein@users.noreply.github.com>
2023-11-17 01:33:44 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,5 +9,9 @@ func main() {
  web.Get("/", func(ctx *context.Context) {
   ctx.WriteString("Hello World")
  })
+
+ web.BConfig.Listen.HTTPAddr = "0.0.0.0"
+ web.BConfig.Listen.HTTPPort = 8080
+
  web.Run()
 }
