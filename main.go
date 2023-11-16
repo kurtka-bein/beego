0508f025ab3b1628e561ab00053b81a5aca1cdbb
kurtka-bein <133507984+kurtka-bein@users.noreply.github.com>
2023-11-17 00:36:41 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import "github.com/beego/beego/v2/server/web"
+import (
+ "github.com/beego/beego/v2/server/web"
+ "github.com/beego/beego/v2/server/web/context"
+)
 
 func main() {
-    web.Get("/", func(ctx *web.Context) {
-        ctx.WriteString("Hello World")
-    })
-    web.Run()
+ web.Get("/", func(ctx *context.Context) {
+  ctx.WriteString("Hello World")
+ })
+ web.Run()
 }
